7316a34759418ac7324192be5bdcc979517fb735
HEX-EXEC <hyp3rtsugikuni@gmail.com>
2025-03-04 22:21:12 +0100

    Revert DATABASE_URL to use env var for Render after local testing

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = "postgresql://" + DATABASE_URL[len("postgres://"):]
 
-# Create a synchronous engine with psycopg2
+# Create a synchronous engine with psycopg
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, class_=Session, expire_on_commit=False)
 
